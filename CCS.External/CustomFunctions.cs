9fa19eb1d7c71cb38dfc7207bc7d3783a87aef62
Einar Egilsson <einar@einaregilsson.com>
2009-05-25 19:36:58 +0000

    More restructuring
    git-svn-id: svn://localhost/msc@151 3916aa72-0654-48f7-a0fd-8d652150b913

---
+++ CCS.External/CustomFunctions.cs
@@ -0,0 +1,33 @@
+using PLR;
+using PLR.Runtime;
+
+namespace External {
+    public static class CustomFunctions {
+        public static IAction MyProcess(IAction a) {
+            if (!(a is ChannelSyncAction)) {
+                return a;
+            }
+            ChannelSyncAction c = (ChannelSyncAction)a;
+            c.Name += "ITWORKS";
+            return c;
+        }
+
+        public static bool RestrictIT(IAction a) {
+            return true;
+        }
+
+        public static void Print(string s) {
+            System.Console.WriteLine(s);
+        }
+
+
+        public static void WRITE(string s) {
+            System.Console.WriteLine(s);
+        }
+
+        public static void WRITE(string s, int arg0) {
+            System.Console.WriteLine(s + arg0);
+        }
+
+    }
+}
