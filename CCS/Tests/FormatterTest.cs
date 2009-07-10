f0facabdc66f269082f77f0c9b90ccb3bbf53403
Einar Egilsson <einar@einaregilsson.com>
2009-07-10 14:49:27 +0000

    git-svn-id: svn://localhost/msc@209 3916aa72-0654-48f7-a0fd-8d652150b913

--- CCS/Tests/FormatterTest.cs
+++ CCS/Tests/FormatterTest.cs
@@ -1,54 +1,54 @@
-/**
- * $Id$ 
- * 
- * This file is part of the Process Language Runtime (PLR) 
- * and is licensed under the GPL v3.0.
- * 
- * Author: Einar Egilsson (einar@einaregilsson.com) 
- */
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using NUnit.Framework;
-using CCS.Parsing;
-using PLR.AST;
-using PLR.AST.Processes;
-using PLR.AST.Actions;
-using PLR.AST.Expressions;
-using PLR.AST.Formatters;
+///**
+// * $Id$ 
+// * 
+// * This file is part of the Process Language Runtime (PLR) 
+// * and is licensed under the GPL v3.0.
+// * 
+// * Author: Einar Egilsson (einar@einaregilsson.com) 
+// */
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Text;
+//using NUnit.Framework;
+//using CCS.Parsing;
+//using PLR.AST;
+//using PLR.AST.Processes;
+//using PLR.AST.Actions;
+//using PLR.AST.Expressions;
+//using PLR.AST.Formatters;
 
-namespace CCS.Tests
-{
-    [TestFixture]
-    public class FormatterTest
-    {
-        private ProcessSystem Parse(string source)
-        {
-            MemoryStream ms = new MemoryStream();
-            StreamWriter w = new StreamWriter(ms);
-            w.Write(source);
-            w.Flush();
-            ms.Seek(0, SeekOrigin.Begin);
+//namespace CCS.Tests
+//{
+//    [TestFixture]
+//    public class FormatterTest
+//    {
+//        private ProcessSystem Parse(string source)
+//        {
+//            MemoryStream ms = new MemoryStream();
+//            StreamWriter w = new StreamWriter(ms);
+//            w.Write(source);
+//            w.Flush();
+//            ms.Seek(0, SeekOrigin.Begin);
 
-            Parser p = new Parser(new Scanner(ms));
-            p.Parse();
-            return p.System;
-        }
+//            Parser p = new Parser(new Scanner(ms));
+//            p.Parse();
+//            return p.System;
+//        }
 
-        [Test]
-        public void TestSourceFormatter()
-        {
-            string src = @"
-System_{n,k} = a . b . c . d . 0 + f . 0 | John
-John = 0";
-            ProcessSystem sys = Parse(src);
-            BaseFormatter f = new BaseFormatter();
+//        [Test]
+//        public void TestSourceFormatter()
+//        {
+//            string src = @"
+//System_{n,k} = a . b . c . d . 0 + f . 0 | John
+//John = 0";
+//            ProcessSystem sys = Parse(src);
+//            BaseFormatter f = new BaseFormatter();
 
-            f.Start(sys);
-            string result = f.GetFormatted();
-            Assert.AreEqual(src, result);
-        }
+//            f.Start(sys);
+//            string result = f.GetFormatted();
+//            Assert.AreEqual(src, result);
+//        }
 
-    }
-}
+//    }
+//}
--- CCS/Tests/ParserTest.cs
+++ CCS/Tests/ParserTest.cs
@@ -1,127 +1,127 @@
-/**
- * $Id$ 
- * 
- * This file is part of the Process Language Runtime (PLR) 
- * and is licensed under the GPL v3.0.
- * 
- * Author: Einar Egilsson (einar@einaregilsson.com) 
- */
- ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using NUnit.Framework;
-using CCS.Parsing;
-using PLR.Analysis;
-using PLR.AST.Processes;
-using PLR.AST.Actions;
-using PLR.AST.Expressions;
-using PLR.AST;
+///**
+// * $Id$ 
+// * 
+// * This file is part of the Process Language Runtime (PLR) 
+// * and is licensed under the GPL v3.0.
+// * 
+// * Author: Einar Egilsson (einar@einaregilsson.com) 
+// */
+// ﻿using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Text;
+//using NUnit.Framework;
+//using CCS.Parsing;
+//using PLR.Analysis;
+//using PLR.AST.Processes;
+//using PLR.AST.Actions;
+//using PLR.AST.Expressions;
+//using PLR.AST;
 
-namespace CCS.Tests {
-    [TestFixture]
-    public class ParserTest {
+//namespace CCS.Tests {
+//    [TestFixture]
+//    public class ParserTest {
 
-        [Test]
-        public void TestNilProcess() {
-            ProcessSystem sys = Parse("P = 0");
-            Assert.AreEqual(1, sys.Count);
-            Process p = sys[0].Process;
-            Assert.IsTrue(p is NilProcess);
-            AssertNoRelabelling(p);
-            AssertNoRestrictions(p);
-            AssertParenCount(p, 0);
-        }
+//        [Test]
+//        public void TestNilProcess() {
+//            ProcessSystem sys = Parse("P = 0");
+//            Assert.AreEqual(1, sys.Count);
+//            Process p = sys[0].Process;
+//            Assert.IsTrue(p is NilProcess);
+//            AssertNoRelabelling(p);
+//            AssertNoRestrictions(p);
+//            AssertParenCount(p, 0);
+//        }
 
-        [Test]
-        public void TestActionPrefixSingleAction() {
-            ProcessSystem sys = Parse("P = john.0");
-            Assert.AreEqual(1, sys.Count);
-            Process p = sys[0].Process;
-            Assert.IsTrue(p is ActionPrefix);
-            ActionPrefix ap = (ActionPrefix)p;
-            Assert.IsTrue(ap.Process is NilProcess);
-            Assert.AreEqual("john", ap.Action.Name);
-            Assert.IsTrue(ap.Action is InAction);
-            AssertNoRelabelling(ap);
-            AssertNoRestrictions(ap);
-            AssertParenCount(ap, 0);
-        }
+//        [Test]
+//        public void TestActionPrefixSingleAction() {
+//            ProcessSystem sys = Parse("P = john.0");
+//            Assert.AreEqual(1, sys.Count);
+//            Process p = sys[0].Process;
+//            Assert.IsTrue(p is ActionPrefix);
+//            ActionPrefix ap = (ActionPrefix)p;
+//            Assert.IsTrue(ap.Process is NilProcess);
+//            Assert.AreEqual("john", ap.Action.Name);
+//            Assert.IsTrue(ap.Action is InAction);
+//            AssertNoRelabelling(ap);
+//            AssertNoRestrictions(ap);
+//            AssertParenCount(ap, 0);
+//        }
 
-        [Test]
-        public void TestActionPrefixMultipleActions() {
-            ProcessSystem sys = Parse("P = john._jane_.(  foo .0)");
-            Assert.AreEqual(1, sys.Count);
-            Process p = sys[0].Process;
-            Assert.IsTrue(p is ActionPrefix);
-            ActionPrefix ap = (ActionPrefix)p;
+//        [Test]
+//        public void TestActionPrefixMultipleActions() {
+//            ProcessSystem sys = Parse("P = john._jane_.(  foo .0)");
+//            Assert.AreEqual(1, sys.Count);
+//            Process p = sys[0].Process;
+//            Assert.IsTrue(p is ActionPrefix);
+//            ActionPrefix ap = (ActionPrefix)p;
 
-            //First action
-            Assert.IsTrue(ap.Process is ActionPrefix);
-            Assert.AreEqual("john", ap.Action.Name);
-            Assert.IsTrue(ap.Action is InAction);
-            AssertNoRelabelling(ap);
-            AssertNoRestrictions(ap);
-            AssertParenCount(ap, 0);
+//            //First action
+//            Assert.IsTrue(ap.Process is ActionPrefix);
+//            Assert.AreEqual("john", ap.Action.Name);
+//            Assert.IsTrue(ap.Action is InAction);
+//            AssertNoRelabelling(ap);
+//            AssertNoRestrictions(ap);
+//            AssertParenCount(ap, 0);
 
-            //Second action
-            ap = (ActionPrefix)ap.Process;
-            Assert.IsTrue(ap.Process is ActionPrefix);
-            Assert.AreEqual("jane", ap.Action.Name);
-            Assert.IsTrue(ap.Action is OutAction);
-            AssertNoRelabelling(ap);
-            AssertNoRestrictions(ap);
-            AssertParenCount(ap, 0);
+//            //Second action
+//            ap = (ActionPrefix)ap.Process;
+//            Assert.IsTrue(ap.Process is ActionPrefix);
+//            Assert.AreEqual("jane", ap.Action.Name);
+//            Assert.IsTrue(ap.Action is OutAction);
+//            AssertNoRelabelling(ap);
+//            AssertNoRestrictions(ap);
+//            AssertParenCount(ap, 0);
 
-            //Third action
-            ap = (ActionPrefix)ap.Process;
-            Assert.IsTrue(ap.Process is NilProcess);
-            Assert.AreEqual("foo", ap.Action.Name);
-            Assert.IsTrue(ap.Action is InAction);
-            AssertNoRelabelling(ap);
-            AssertNoRestrictions(ap);
-            AssertParenCount(ap, 1);
-        }
+//            //Third action
+//            ap = (ActionPrefix)ap.Process;
+//            Assert.IsTrue(ap.Process is NilProcess);
+//            Assert.AreEqual("foo", ap.Action.Name);
+//            Assert.IsTrue(ap.Action is InAction);
+//            AssertNoRelabelling(ap);
+//            AssertNoRestrictions(ap);
+//            AssertParenCount(ap, 1);
+//        }
 
-        private void AssertParenCount(Process p, int parens) {
-            Assert.AreEqual(parens, p.ParenCount);
-        }
+//        private void AssertParenCount(Process p, int parens) {
+//            Assert.AreEqual(parens, p.ParenCount);
+//        }
 
-        private void AssertNoRestrictions(Process p) {
-            Assert.AreEqual(0, p.ActionRestrictions.Count);
-        }
-        private void AssertNoRelabelling(Process p) {
-            Assert.AreEqual(0, p.PreProcessActions.Count);
-        }
-        private ProcessSystem Parse(string source) {
-            MemoryStream ms = new MemoryStream();
-            StreamWriter w = new StreamWriter(ms);
-            w.Write(source);
-            w.Flush();
-            ms.Seek(0, SeekOrigin.Begin);
+//        private void AssertNoRestrictions(Process p) {
+//            Assert.AreEqual(0, p.ActionRestrictions.Count);
+//        }
+//        private void AssertNoRelabelling(Process p) {
+//            Assert.AreEqual(0, p.PreProcessActions.Count);
+//        }
+//        private ProcessSystem Parse(string source) {
+//            MemoryStream ms = new MemoryStream();
+//            StreamWriter w = new StreamWriter(ms);
+//            w.Write(source);
+//            w.Flush();
+//            ms.Seek(0, SeekOrigin.Begin);
 
-            Parser p = new Parser(new Scanner(ms));
-            p.Parse();
-            return p.System;
-        }
+//            Parser p = new Parser(new Scanner(ms));
+//            p.Parse();
+//            return p.System;
+//        }
 
-        [Test]
-        public void TestArithmeticExpression() {
-            TestAExp("3+4", 7);
-            TestAExp("3*4", 12);
-            TestAExp("3/4", 0);
-            TestAExp("3-1", 2);
-            TestAExp("5+3+(3*4/2+4%2)-23*2-2-(3*4+1)", -47);
-            TestAExp("56*(65+2*26*34+2*356-(23*2-5))", 140224);
-        }
+//        [Test]
+//        public void TestArithmeticExpression() {
+//            TestAExp("3+4", 7);
+//            TestAExp("3*4", 12);
+//            TestAExp("3/4", 0);
+//            TestAExp("3-1", 2);
+//            TestAExp("5+3+(3*4/2+4%2)-23*2-2-(3*4+1)", -47);
+//            TestAExp("56*(65+2*26*34+2*356-(23*2-5))", 140224);
+//        }
 
-        private void TestAExp(string exp, int expectedValue) {
-            ProcessSystem sys = Parse(@"P = X_{" + exp + "}");
-            Assert.AreEqual(1, sys.Count);
-            ProcessConstant p = (ProcessConstant)sys[0].Process;
-            ArithmeticExpression expr = (ArithmeticExpression)p.Expressions[0];
-            //Assert.AreEqual(expectedValue, expr.Value);
-        }
+//        private void TestAExp(string exp, int expectedValue) {
+//            ProcessSystem sys = Parse(@"P = X_{" + exp + "}");
+//            Assert.AreEqual(1, sys.Count);
+//            ProcessConstant p = (ProcessConstant)sys[0].Process;
+//            ArithmeticExpression expr = (ArithmeticExpression)p.Expressions[0];
+//            //Assert.AreEqual(expectedValue, expr.Value);
+//        }
 
-    }
-}
+//    }
+//}
