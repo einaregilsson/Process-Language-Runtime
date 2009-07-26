4a7a3378d0f36b661dfecf2fc5829d8ac9f39dd2
Einar Egilsson <einar@einaregilsson.com>
2009-07-26 20:38:01 +0000

    Presentation ready
    git-svn-id: svn://localhost/msc@235 3916aa72-0654-48f7-a0fd-8d652150b913

--- CCS.ATM/Bank.Functions.cs
+++ CCS.ATM/Bank.Functions.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Bank {
     public class Functions {
         public static bool ValidPin(int accountNr, int pinNr) {
             //Here we could have something interfacing with the banks
             //actual software...
-            return true;
+            DialogResult result = MessageBox.Show("Is the pin " + pinNr + " valid for the account nr " + accountNr + "?", "Validate PIN", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return result == DialogResult.Yes;
         }
 
         public static bool WithDrawFromAccount(int accountNr, int amount) {
